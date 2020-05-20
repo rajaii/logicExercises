9f8499cda5dae4f063f751efc4d706b1584bd36c
Ali Rajaii <rajaii_a@yahoo.com>
2020-05-20 14:56:25 -0400

    first pass

---
+++ javascript/findWitinArray.js
@@ -0,0 +1,11 @@
+//findWithinArray from codewars 6 kyu https://www.codewars.com/kata/51f082ba7297b8f07f000001/train/javascript
+
+var findInArray = function(array, iterator) {
+    for (let i = 0; i< array.length; i++) {
+      if (iterator(array[i], i) === true) {
+      return i
+      }
+    } 
+    return -1
+    
+  };
--- javascript/greetMe.js
+++ javascript/greetMe.js
@@ -1,4 +1,5 @@
 //take a name in your function and greet the name capitalized
+//greet me codewars 7kyu https://www.codewars.com/kata/535474308bb336c9980006f2/train/javascript
 
 var greet = function(name) {
     let newName = name[0].toUpperCase()  
