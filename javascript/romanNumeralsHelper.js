6b28d68789e2b79e2aa21766851f695e0fbbbff5
Ali Rajaii <rajaii_a@yahoo.com>
2020-07-10 10:00:21 -0400

    added finish code for romanNumeralsHelper.

--- javascript/romanNumeralsHelper.js
+++ javascript/romanNumeralsHelper.js
@@ -10,10 +10,18 @@ let romans = {
     "D": 500,
     "M": 1000
   }
+
+  let nums = {'M': 1000, 'CM': 900, 'D': 500, 'CD': 400, 'C': 100, 'XC': 90, 'L': 50, 'XL': 40, 'X': 10, 'IX': 9, 'V': 5, 'IV': 4, 'I': 1}
   
   class Romans {
     toRoman(i) {
-      return 'hi'
+      let retStr = ''
+      for (let key in nums) {
+        while (i > nums[key]) {
+          retStr += key
+          i -= nums[key]
+        }
+      } return retStr
     }
     
     fromRoman(r) {
