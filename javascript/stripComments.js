e40decbba0b7b1ce90429d5064490d5b7890cc31
Ali Rajaii <rajaii_a@yahoo.com>
2020-08-19 10:45:16 -0400

    stripComments.js Logic was right all along, was a misunderstanding about how JS reads \n in a string line 15 shows it will read input[i-1] to be \n not input[i] === 'n' && input[i-1] === '\'.  You live you learn

--- javascript/stripComments.js
+++ javascript/stripComments.js
@@ -1,49 +1,50 @@
 //from codeWars 4kyu https://www.codewars.com/kata/51c8e37cee245da6b40000bd/train/javascript
 
 function solution(input, markers) {
-    //make an obj so have o(1) lookup and not use o(n) .includes()
-    const markerObj = {};
-    for (let i = 0; i < markers.length; i++) {
-      markerObj[markers[i]] = markers[i];
-    }
-    let retStr = '';
-    let isStripping = false;
-    let start = 0;
-    //loop through input and append to retStr all that is not to be stripped
-    for (let i = 0; i < input.length; i++) {
-                                                                    //V
-       if (isStripping === true && input[i] === 'n' && input[i-1] === '\\') {
-           start = i-1
-           isStripping = false;
-           } 
-           //"apples, plums % and bananas                  \npears\noranges       !applesauce", ["%", "!"]
-       else if (markers[input[i]] != undefined && markers[input[i]] === input[i]) {
-        if (input[i-1] === ' ') {
-            let whiteSpaceCounter = 0;
-            let j = i-1;
-            while (input[j] === ' ') {
-              whiteSpaceCounter += 1;
-              j -= 1;
-            }
-          retStr += input.slice(start, i-whiteSpaceCounter);
-          isStripping = true;
-          continue
-      } else {
-        retStr += input.slice(start, i)
+  //make an obj so have o(1) lookup and not use o(n) .includes()
+  const markerObj = {};
+  for (let i = 0; i < markers.length; i++) {
+    markerObj[markers[i]] = markers[i];
+  }
+  let retStr = '';
+  let isStripping = false;
+  let start = 0;
+  //loop through input and append to retStr all that is not to be stripped
+ for (let i = 0; i < input.length; i++) {
+    
+     if (isStripping === true && input[i-1] === '\n') {
+         start = i-1
+         isStripping = false;
+         } 
+
+     else if (markerObj[input[i]] != undefined && markerObj[input[i]] === input[i]) {
+      if (input[i-1] === ' ') {
+          let whiteSpaceCounter = 0;
+          let j = i-1;
+          while (input[j] === ' ') {
+            whiteSpaceCounter += 1;
+            j -= 1;
+          }
+        retStr += input.slice(start, i-whiteSpaceCounter);
         isStripping = true;
-        continue;
-      }
-         
-    } else if (isStripping === true && i === input.length - 1) {
-      break
-    }  else if (isStripping === false && i === input.length - 1) {
-      retStr += input.slice(start)
+        continue
+    } else {
+      retStr += input.slice(start, i)
+      isStripping = true;
+      continue;
     }
-     
+       
+  } else if (isStripping === true && i === input.length - 1) {
+    break
+  }  else if (isStripping === false && i ===input.length - 1) {
+    retStr += input.slice(start)
   }
-   return retStr
-  }
- console.log('\\') 
+   
+}
+ return retStr
+}
+
+ 
   
   
   //"apples, plums % and bananas\npears\noranges%"  ["%", "!"]
