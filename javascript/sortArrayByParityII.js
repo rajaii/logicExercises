2ad9b0856683fad53d2fb0f8598b0e45bcbe9fda
Ali Rajaii <rajaii_a@yahoo.com>
2020-10-28 10:21:19 -0400

    sortArrayByParity.js refactor a little better on mempry and space, and learned a new tactic

--- javascript/sortArrayByParityII.js
+++ javascript/sortArrayByParityII.js
@@ -1,29 +1,25 @@
 //from leetcode: easy: https://leetcode.com/problems/sort-array-by-parity-ii/
 
 var sortArrayByParityII = function(a) {
-    //set the variables for memeory on odds/evens and return value
+    //set an array of the right length with empty values to add into
     let retArr = [];
-    let evens = [];
-    let odds = [];
+    retArr.length = a.length;
     
-    //loop a and push into evens or odds
-    for (let i = 0; i < a.length; i++) {
+    //loop a once pushing into retArr[j] if they are even and incrementing j by 2, then same       //for odds in another loop
+     let j = 0;
+     for (let i = 0; i < a.length; i++) {
         if (a[i] % 2 === 0) {
-            evens.push(a[i]);
-        } else {
-            odds.push(a[i]);
-        }
+            retArr[j] = a[i];
+            j += 2;
+        } 
     }
-    
-    //if even.length > odds.length loop evens pushing evens[i] into retArr then odds[i] into 
-    //retArr if it exists.  If odds has a bigger length then same logic flipped to run on odds
-    // if (evens.length > odds.length) {
-        for (let i = 0; i < evens.length; i++) {
-            retArr.push(evens[i]);
-            retArr.push(odds[i]);
-            
-        }
-    
+    j = 1;
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] % 2 === 1) {
+            retArr[j] = a[i];
+            j += 2; 
+        } 
+    }
     return retArr;
-    
+   
 };
