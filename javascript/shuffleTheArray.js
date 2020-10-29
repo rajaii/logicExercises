69ff7e0358a185ce1939dfd4dfe490d745ca8d33
Ali Rajaii <rajaii_a@yahoo.com>
2020-10-29 10:15:23 -0400

    refactor shuffleTheArray.js faster on runtime

--- javascript/shuffleTheArray.js
+++ javascript/shuffleTheArray.js
@@ -1,20 +1,28 @@
 //from leetcode easy https://leetcode.com/problems/shuffle-the-array/
 
 var shuffle = function(nums, n) {
-    //split nums in half
-    let num1 = nums.slice(0,n);
-    let num2 = nums.slice(n);
-    
-    //loop num1 and push num1[i] then num2[i] into retArr
+    //loop nums and push num[i then num[n+i] into retArr
     let retArr = [];
-    for (let i = 0; i < num1.length; i++) {
-        retArr.push(num1[i]);
-        retArr.push(num2[i]);
+    for (let i = 0; i < n; i++) {
+        retArr.push(nums[i]);
+        retArr.push(nums[n+i]);
     }
     
-    return retArr; 
-    
+    return retArr;
+       
 };
+
+//edge cases:
+//?'s:limits on time/space?  
+
+//split the array in two halves
+//loop one half and then push a1[i] then a2[i] into retArr
+//return retArr
+
+//refactor
+//loop to n
+//push into retArr nums[i]
+//push into retARr nums[n+i]
 
 //edge cases:
 //?'s:limits on time/space?  
