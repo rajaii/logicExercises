de8a9272547381a66172b95854a1361a61719a43
Ali Rajaii <rajaii_a@yahoo.com>
2020-11-05 10:35:14 -0500

    converAHexStringToRGB.py refactor save some space

--- python/convertAHexStringToRGB.py
+++ python/convertAHexStringToRGB.py
@@ -1,16 +1,13 @@
 #from codewars 5kyu https://www.codewars.com/kata/5282b48bb70058e4c4000fa7/train/python
 
 def hex_string_to_RGB(hex_string): 
-    #set the dict to add values into, and r g anb b strings
+    #set dict to add colors and return
     rgb = {}
-    r = hex_string[1:3]
-    g = hex_string[3:5]
-    b = hex_string[5:]
     
     #append appropriate values to dict and return it
-    rgb['r'] = int(r, 16)
-    rgb['g'] = int(g, 16)
-    rgb['b'] = int(b, 16)
+    rgb['r'] = int(hex_string[1:3], 16)
+    rgb['g'] = int(hex_string[3:5], 16)
+    rgb['b'] = int(hex_string[5:], 16)
     
     return rgb
    
