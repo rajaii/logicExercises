18961504332fe7a357f7497fe912f8c8b15db54b
Ali Rajaii <rajaii_a@yahoo.com>
2020-11-10 10:48:57 -0500

    selfDividingNumbers.js first pass

---
+++ javascript/selfDividingNembers.js
@@ -0,0 +1,45 @@
+//from leetcode easy https://leetcode.com/problems/self-dividing-numbers
+
+var selfDividingNumbers = function(left, right) {
+    //set the array to return
+    let retArr = [];
+    let s;
+    
+    //loop from left to right checking if each num is self-dividing, and pushing into retArr if so
+    for (let i = left; i <= right; i++) {
+        s = i.toString();
+        if (s.includes('0')) {
+            continue;
+        } else {
+            for (let j = 0; j < s.length; j++) {
+                if (parseInt(s, 10) % parseInt(s[j], 0) != 0) {
+                    break;
+                } else if (j === s.length - 1 && (parseInt(s, 10) % parseInt(s[j],10)) === 0) {
+                 
+                    retArr.push(parseInt(s, 10));
+                }
+            }
+        }
+    }
+    
+    return retArr;
+    
+};
+
+//?'s: limits on time/space? 
+//edge cases: left = right,
+
+//set retArr= []
+//loop from left to right (including right)
+
+//s = toString each num
+ 
+//if (s.includes('0'))
+//continue
+//else loop to s.length
+//if (parseInt(s,10) % s[i] != 0)
+//break
+//else if (i === s.length && parsetInt(s, 10) % s[i] === 0)
+//retArr.push(parseInt(s,10))
+
+//return retArr
