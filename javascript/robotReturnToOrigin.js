559515c0165f154a86e49b71fb3cf99c68f9591f
Ali Rajaii <rajaii_a@yahoo.com>
2020-12-02 17:13:17 -0500

    refactor and clean robotReturnToOrigin.js

--- javascript/robotReturnToOrigin.js
+++ javascript/robotReturnToOrigin.js
@@ -13,17 +13,12 @@ var judgeCircle = function(moves) {
            x -= 1;        
         } else if (moves[i] === 'U') {
            y += 1;        
-        } else if (moves[i] === 'D') {
+        } else {
            y -= 1;        
         }
     }
     
-    //return true && x===0 && y === 0
-    if (x === 0 && y === 0) {
-        return true
-    } else {
-        return false
-    }
+    return true && x === 0 && y === 0;
     
 };
 
