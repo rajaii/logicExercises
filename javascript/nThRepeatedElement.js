8acba06a6156634abfc3758e0cd73e55d2f83b38
Ali Rajaii <rajaii_a@yahoo.com>
2020-12-07 17:54:02 -0500

    nThRepeatedElement.js refactor: 88 MS runtime; 41.2 MB space

--- javascript/nThRepeatedElement.js
+++ javascript/nThRepeatedElement.js
@@ -1,17 +1,21 @@
 //from leetcode easy: https://leetcode.com/problems/n-repeated-element-in-size-2n-array/submissions/
 
 var repeatedNTimes = function(a) {
-    //make the object to store a[i] and counts, in making the object if you have the key return that value
-     let store = {};
-     for (let i = 0; i < a.length; i++) {
-         if (store[a[i]] != undefined) {
-             return a[i]
+    //set visited
+     let visited = [];
+     let y = a.length;
+     
+     //loop a and push into visited, if a[i] is in visited, return a[i]
+     for (let i = 0; i < y; i++) {
+         if (visited.includes(a[a.length - 1])) {
+             return a[a.length - 1];
          } else {
-             store[a[i]] = 1;
+             visited.push(a.pop());
          }
      }
      
  };
+ 
  
  //Edge Cases: 
  //?'s: are all elements numbers? limits on time/space?
@@ -24,3 +28,7 @@ var repeatedNTimes = function(a) {
  //Refactor to improve runtime at the expense of space:
  //make an object of the form (a[i], count)
  //if obj[ai] exists, return a[i]
+
+ //Refactor
+//use visited, 
+//loop a and push into visted
