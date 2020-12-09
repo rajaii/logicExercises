7eaeed516e9190d1e75745a279912ad39bca5714
Ali Rajaii <rajaii_a@yahoo.com>
2020-12-09 08:54:39 -0500

    richestCustomerWealth.js refactor runtime: 78 MS, space: 38.6 MB

--- javascript/richestCustomerWealth.js
+++ javascript/richestCustomerWealth.js
@@ -1,17 +1,5 @@
 //From leetcode easy https://leetcode.com/problems/richest-customer-wealth/submissions/
 
 var maximumWealth = function(a) {
-    //set maximum
-    let maximum = 0;
-    let temp = 0;
-    //loop accounts taking the sum of a[i] and setting to max if it is greater than current maximum
-    for (let i = 0; i < a.length; i++) {
-        temp += a[i].reduce((a,c) => a + c, 0);
-        if (temp > maximum) {
-            maximum = temp
-        }
-        temp = 0;
-    }
-    
-    return maximum
+    return Math.max(...a.map(n => n.reduce((a,c) => a + c, 0)));
 };
