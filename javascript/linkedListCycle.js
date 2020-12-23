477843893eaa0cd284177bd6fb483a17d9a8ae01
Ali Rajaii <rajaii_a@yahoo.com>
2020-12-23 10:22:06 -0500

    linkedListCycle.js refactor still O(n) space and time but 72MS 41 MB

--- javascript/linkedListCycle.js
+++ javascript/linkedListCycle.js
@@ -1,21 +1,19 @@
 //from leetcode easy: https://leetcode.com/problems/linked-list-cycle/
 
 var hasCycle = function(head) {
-    //traverse the linked list resetting the val to an object and if you run into an         //object return false
-    let cur = head;
-    while (cur != null) {
-        if (typeof cur.val === 'object') {
-            return true;
-        }
-        
-        cur.val = {
-            val: cur.val
-        }
-        cur = cur.next;
-    }
-    
-    return false;
-};
+       //traverse the linked list resetting the val to 'hi' and if you run into a str           //return true
+       let cur = head;
+       while (cur != null) {
+           if (cur.val === 'hi') {
+               return true;
+           }
+           cur.val = 'hi'
+           cur = cur.next;
+       }
+       
+       return false;
+   };
+   
 
 //edge cases: head === null, head.next == null
 //?'s: limits on space, limits on time?
