6899992b3d49e4f8460288393b462f022f666442
Ali Rajaii <rajaii_a@yahoo.com>
2021-01-28 10:07:34 -0500

    flippingAnImage.js 88MS runtime 44 MB O(n^2) runtime complexity and constant space

--- javascript/flippingAnImage.js
+++ javascript/flippingAnImage.js
@@ -1,19 +1,17 @@
 //from leetcode easy https://leetcode.com/problems/flipping-an-image/
 
 var flipAndInvertImage = function(A) {
-    //reverse each row
-    let a = A.map(ar => ar.reverse())
-    
-    //flip the bits
-    for (let i = 0; i < a.length; i++) {
-        for (let j = 0; j < a[i].length; j++) {
-            if (a[i][j] === 0) {
-                a[i][j] = 1;
+    return a = A.map(ar => {
+        ar.reverse();
+        for (let i = 0; i < ar.length; i++) {
+            if (ar[i] === 0) {
+                ar[i] = 1;
             } else {
-                a[i][j] = 0;
+                ar[i] = 0;
             }
         }
-    }
-    
-    return a
+        return ar;
+    }); 
 };
+
+
