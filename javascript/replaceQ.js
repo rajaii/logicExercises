abe54d42dd238d8f0e6759a0b9d184a0ed18115f
Ali Rajaii <rajaii_a@yahoo.com>
2021-02-24 10:31:41 -0500

    replaceQ.js 104MS 40.3MB O(n^2) runtime complexity O(n) space complexity

---
+++ javascript/replaceQ.js
@@ -0,0 +1,51 @@
+//from leetcode easy: https://leetcode.com/problems/replace-all-s-to-avoid-consecutive-repeating-character
+
+var modifyString = function(str) {
+    //set the alphabet array to check against in the loop and split str
+    let alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    let s = str.split('');
+    
+    
+    //loop s sub-looping alpha if ? arises, swapping with letters that don't lead to repeats 
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '?') {
+            for (let j = 0; j < alpha.length; j++) {
+                if (i === 0 && s[i+1] != alpha[j]) {
+                    s[i] = alpha[j];
+                    break;
+                } else if (i === s.length - 1 && s[i-1] != alpha[j]) {
+                    s[i] = alpha[j];
+                    break;
+                } else {
+                    if (s[i-1] != alpha[j] && s[i+1] != alpha[j]) {
+                        s[i] = alpha[j];
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    return s.join('');
+};
+
+//edge cases: length 1. ? at beginning or end(loop logic) 
+//?'s: limits on time/space?  
+
+//PSEUDO:
+//set alpha array
+//set s = str.split('')
+//loop s
+//if s[i] === '?'
+//loop alpha
+//if (i === 0 && s[i+1] != alpha[j]):
+//s[i] = alpha[j]
+//break
+//else if (i === s.length - 1 && s[i-1] != alpha[j]) {
+//s[i] = alpha[j]
+//break
+//else
+//if (s[i-1] != alpha[j] && s[i+1] != alpha[j])
+//s[i] = alpha[j]
+
+//return s.join('')
