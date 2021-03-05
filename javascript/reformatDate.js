c437dcad321040b6f780d01e8cebf506f299ac39
Ali Rajaii <rajaii_a@yahoo.com>
2021-03-05 10:53:57 -0500

    reformatDate.js 80MS 39MB O(1) runtime complexity O(1) space complexity

--- javascript/reformatDate.js
+++ javascript/reformatDate.js
@@ -2,16 +2,15 @@
 
 var reformatDate = function(date) {
     //make the monthObject to equivocate and retDate to concat and return
-    const monthObj = {"Jan": '01', "Feb": '02', "Mar": '03', "Apr": '04', "May": '05', "Jun": '06', "Jul": '07', "Aug": '08', "Sep": '09', "Oct": '10', "Nov": '11', "Dec": '12'}
+    const monthObj = {"Jan": '01', "Feb": '02', "Mar": '03', "Apr": '04', "May": '05', "Jun": '06', "Jul": '07', "Aug": '08', "Sep": '09', "Oct": '10', "Nov": '11', "Dec": '12'};
     let retDate = '';
     
     //split d and concat to retDate accordingly
     const d = date.split(' ');
-    retDate += `${d[2]}-`
-    retDate += `${monthObj[d[1]]}-`  
+    retDate += `${d[2]}-`;
+    retDate += `${monthObj[d[1]]}-`;  
    
     //split d[0] to get rid of the letters and only use the numbers
-    
     let regex = /[snrt]/;
     let day = d[0].split(regex);
     if (day[0].length > 1) {
