9e67223887c409e824008eb10c9390daeef99f8d
Ali Rajaii <rajaii_a@yahoo.com>
2021-03-26 10:25:47 -0400

    binarySearch.js refactored to not use built ins O(n) time complexity O(1) space 88MS 42MB

--- javascript/binarySearch.js
+++ javascript/binarySearch.js
@@ -1,9 +1,37 @@
 //from leetcode: easy: https://leetcode.com/problems/binary-search/
 
+// var search = function(nums, target) {
+//     if (!nums.includes(target)) {
+//         return -1
+//     }
+    
+//     return nums.indexOf(target);
+// };
+
+//did above in less than a min using build in fncs, will now try to do without them
+//edge cases: 1 num in nums, 
+//?'s: limits on time/space?
+
+//PSEUDO:
+
+//loop nums
+//if nums[i] === target:
+//return i
+
+//outside of loop return -1
+
 var search = function(nums, target) {
-    if (!nums.includes(target)) {
-        return -1
+    //loop nums to check if target is in there
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === target) {
+            return i;
+        }
     }
     
-    return nums.indexOf(target) || -1;
+    return -1
 };
+
+
+
+
+
