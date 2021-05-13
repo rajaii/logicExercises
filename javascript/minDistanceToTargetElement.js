e180f877ce79b99cae353fdbf8b859bc4b13ebea
Ali Rajaii <rajaii_a@yahoo.com>
2021-05-13 10:17:42 -0400

    refactor minDistanceToTargetElement same runtime o(1) space

--- javascript/minDistanceToTargetElement.js
+++ javascript/minDistanceToTargetElement.js
@@ -1,23 +1,16 @@
 //  from leetcode easy: https://leetcode.com/problems/minimum-distance-to-the-target-element
 
 var getMinDistance = function(nums, target, start) {
-    //  set index's in memory
+    //  set min in memory
     let min = Infinity;
-    const indexs = [];
     
-    //  loop nums and pull out the index's of all nums[i] that === target
+    //  loop nums and if nums[i] is target check and see if it's abs diff from start     //  is less than min and set min to that if so
     for (let i = 0; i < nums.length; i++) {
-        if (nums[i] === target) {
-            indexs.push(i);
+        if (nums[i] === target && Math.abs(i - start) < min) {
+            min = Math.abs(i - start);
         }
     }
     
-    //  loop indexs and set min to Math.abs(index's[i] - start) if less than min
-    for (let i = 0; i < indexs.length; i++) {
-        if (Math.abs(indexs[i] - start) < min) {
-            min = Math.abs(indexs[i] - start);
-        } 
-    }
     
     return min;
 };
