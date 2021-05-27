40e730aa07c525e53df39705887af8048b7c0a49
Ali Rajaii <rajaii_a@yahoo.com>
2021-05-27 17:14:08 -0400

    media query generator

--- javascript/x.js
+++ javascript/x.js
@@ -1,2 +1,21 @@
+// let width = 280;
+// let left = 114;
+// for (let i = 375; i < 769; i++) {
+//   console.log(`@media (min-width: ${i-1}px) and (max-width: ${i + 1}px) { width: ${width}px; left: ${left}px; }`);
+//   width += 1;
+//   left += 1;
+// }
 
+
+let width = 385;
+let top = 457;
+for (let i = 451; i < 495; i++) {
+  console.log(`@media (min-width: ${i-1}px) and (max-width: ${i + 1}px) { width: ${width}px; top: ${top}px}`);
+  width += 1;
+  top += 1;
+}
+
+
+// for right left up 1 width up 1
+//for left try w up 1
 
