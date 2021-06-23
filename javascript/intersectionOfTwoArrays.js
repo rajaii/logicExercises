af7f7619db947e8d89e8062f74d2adbd5ecabe42
Ali Rajaii <rajaii_a@yahoo.com>
2021-06-23 11:48:56 -0400

    intersectionOfTwoArrays.js refactor O(n log n) runtime o(n) space

--- javascript/intersectionOfTwoArrays.js
+++ javascript/intersectionOfTwoArrays.js
@@ -32,3 +32,54 @@ var intersection = function(nums1, nums2) {
 //find arr with larger length
 //loop that array (double looping smaller one) and check for overlaps and push into //intersection
 //return intersection
+
+//refactor to improve runtime:
+//sort both arrays
+//remove duplicates:
+//set num1b and num2b
+//loop num1 and num2 and push into the b's if num1b[i - 1] !== undefined && num1b[i //- 1] !== num1b[1]
+
+//set intersections
+//concat the arrays, sort the concatenated array and remove the duplicates of the //sorrted, concatenated array by doint the above but pushing into intersections
+
+//return intersections O(n log n) time
+
+var intersection = function(nums1, nums2) {
+    //  sort both arrays
+    nums1.sort((a,b) => a - b);
+    nums2.sort((a,b) => a - b);
+    
+    //  remove duplicates
+    let nums1b = [];
+    let nums2b = [];
+    for (let i = 0; i < nums1.length; i++) {
+        if (i === 0) {
+            nums1b.push(nums1[i]);
+        }
+        if (nums1[i - 1] !== undefined && nums1[i - 1] !== nums1[i]) {
+            nums1b.push(nums1[i]);
+        }
+    }
+    
+    for (let i = 0; i < nums2.length; i++) {
+        if (i === 0) {
+            nums2b.push(nums2[i]);
+        }
+        if (nums2[i - 1] !== undefined && nums2[i - 1] !== nums2[i]) {
+            nums2b.push(nums2[i]);
+        }
+    }
+     
+    //  concat the sorted and sort and remove duplicates
+    let intersection = [];
+    let whole = nums1b.concat(nums2b);
+    whole.sort((a,b) => a - b);
+    
+    for (let i = 0; i < whole.length; i++) {
+        if (whole[i - 1] !== undefined && whole[i - 1] === whole[i]) {
+            intersection.push(whole[i]);
+        }
+    }
+    
+    return intersection;
+};
