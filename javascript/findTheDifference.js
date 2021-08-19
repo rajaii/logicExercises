f3f4b5d4b2e72b9d7d3d938d9666491b104a7e83
Ali Rajaii <alirajaii@alis-mbp.lan>
2021-08-19 09:04:41 -0500

    findTheDifference.js o(n^2) runtime o(n) space

--- javascript/findTheDifference.js
+++ javascript/findTheDifference.js
@@ -57,3 +57,45 @@ var findTheDifference = function(s, t) {
 // loop tObj keys and if !sObj.includes(Object.keys(tObj)[i]):
 // return Object.keys(tObj)[i]
 // loop through Object.values(sObj) under above loop and find the Object.values(tObj[i]) that is greater than s's 
+
+
+
+
+//refactor space tArrK just call
+// if (tObj[key] > sObj[key] || sOnbj[key] === undefined) { may get rid of need for first loop
+
+
+var findTheDifference = function(s, t) {
+    // handle edge cases
+    if (s === "") {
+        return t[0];
+    }
+    else {
+        //  make objects to store the substrings and counts
+        const sObj = {};
+        const tObj = {};
+        for (let i = 0; i < s.length; i++) {
+            if (sObj[s[i]] === undefined) {
+                sObj[s[i]] = 1;
+            } else {
+                sObj[s[i]] += 1;
+            }
+        }
+        
+        for (let i = 0; i < t.length; i++) {
+            if (tObj[t[i]] === undefined) {
+                tObj[t[i]] = 1;
+            } else {
+                tObj[t[i]] += 1;
+            }
+        }
+        
+        
+        //.  loop the key,val on tObj and if any val is greater than sObj at val return the key
+        for (let key in tObj) {
+            if (tObj[key] > sObj[key] || sObj[key] === undefined) {
+                return key;
+            }
+        }
+    }
+};
