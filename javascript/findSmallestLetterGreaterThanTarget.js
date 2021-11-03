bb7a458ce06c4705c800c77f568d6f9642c9bfe5
Ali Rajaii <rajaii_a@yahoo.com>
2021-11-03 11:26:26 -0500

    findSmallestLetterGreaterThanTarget.js o(n) runtime O(1) spacee

---
+++ javascript/findSmallestLetterGreaterThanTarget.js
@@ -0,0 +1,28 @@
+//  from leetcode easy: https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+
+
+var nextGreatestLetter = function(letters, target) {
+    if (!letters.includes(target)) {
+    if (target > letters[letters.length-1]) {
+        return letters[0];
+    }      
+    for (let i = 0; i < letters.length; i++) { 
+        if (letters[i] > target) {
+            return letters[i]
+        }
+    }
+    } 
+        let index = letters.indexOf(target);
+        if (letters[letters.length - 1] === target) {
+            for (let i = 0; i < letters.length; i++) {
+                if ( letters[i] !== target) {
+                    return letters[i]
+                }
+            }
+        }      
+        for (let i = index; i < letters.length; i++) {
+            if (letters[i] !== target) {
+                return letters[i];
+            }
+        }
+};
