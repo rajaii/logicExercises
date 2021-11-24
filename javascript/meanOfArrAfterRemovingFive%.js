802e01c19e5169a645f011d141580f03744c60d1
Ali Rajaii <rajaii_a@yahoo.com>
2021-11-24 11:55:27 -0600

    meanOfArrAfterRemvingFive%.js O(n log n) runtime O(n) space

---
+++ javascript/meanOfArrAfterRemovingFive%.js
@@ -0,0 +1,27 @@
+//from leetcode easy: https://leetcode.com/problems/mean-of-array-after-removing-some-elements/
+
+var trimMean = function(arr) {
+    arr.sort((a,b) => a - b);
+    
+    //find out the smallest and largest 5%
+    const whole = arr.length;
+    const five = .05 * whole;
+    const avgArr = []; 
+    
+    //push smallest and largest 5% into avgArr
+    let index;
+    if (five % 1 === 0) {
+        index = five;
+    } else {
+        index = Math.ceil(five);
+    }
+    for (let i = index; i < (arr.length - index); i++) {
+        avgArr.push(arr[i]);
+    } 
+    
+    return avgArr.reduce((a,c) => a+c , 0) / avgArr.length;
+};
+
+//find the smallest and largest 5% based off of index
+
+//reduce on the slice of arr without the top and bottom 5%
