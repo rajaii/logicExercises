57c96988808a6fbb94db7d1ed8ff4f2d9fdc8556
Ali Rajaii <rajaii_a@yahoo.com>
2021-12-31 11:58:23 -0600

    numberCompliment.js o(n)runtime o(n) space

--- javascript/x.js
+++ javascript/x.js
@@ -1,11 +1,4 @@
-const a = new Date("12-23-2021")
-//ms in a day86,400,000
-var day = 60 * 60 * 24 * 1000;
-
-var endDate = new Date(a.getTime() + day)
-console.log(a)
-console.log(endDate.toISOString().slice(0,10))
-console.log(endDate.getTime())
-console.log(`${endDate.toISOString().slice(5,10)}-${endDate.toISOString().slice(0,4)}`)
+const num = "010"
+console.log(parseInt(num, 2))
 
 
