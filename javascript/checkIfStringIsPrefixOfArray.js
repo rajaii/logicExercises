ca80d1686fe47ea26e060cbb8c8cff81c7da8529
Ali Rajaii <rajaii_a@yahoo.com>
2022-01-10 11:26:58 -0600

    checkIfStringIsPrefixOfArray.js o(n) runtime o(1) space

---
+++ javascript/checkIfStringIsPrefixOfArray.js
@@ -0,0 +1,33 @@
+//from leetcode easy: https://leetcode.com/problems/check-if-string-is-a-prefix-of-array/
+
+var isPrefixString = function(str, w) {
+    //set the string to concat to
+    let s = "";
+    
+    //loop words and concat w[i] to s and run checks
+    for (let i = 0; i < w.length; i++) {
+        s += w[i];
+        if (s === str) {
+            return true;
+        }
+        if (s.length > str.length) {
+            return false;
+        }
+    }
+    
+    return false;
+};
+
+//PSEUDO
+
+//set s = '';
+//loop words
+//s + w[i];
+//if (s === str) {
+//return true;
+//}
+//if (s.length > str.length) {
+//return false
+//}
+
+//return false;
