3de425baa42ef666a2d9899543755a64ec3f5442
Ali Rajaii <rajaii_a@yahoo.com>
2022-01-17 11:56:56 -0600

    findNumbersWithEvenNumberOfDigits.js o(n) runtime o(1) space

---
+++ javascript/findNumbersWithEvenNumberOfDigits.js
@@ -0,0 +1,29 @@
+//  from leetcode easy: https://leetcode.com/problems/find-numbers-with-even-number-of-digits
+
+var findNumbers = function(nums) {
+    //set evens
+    let evens = 0;
+    
+    //loop nums and then nums[i].toString() and count number of digits each num has incrementing evens       //accordingly
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i].toString().length % 2 === 0) {
+            evens += 1;
+        }
+     }
+    
+    
+    return evens;
+};
+
+//PSEUDO:
+//set evens = 0
+//loop nums
+//set a counter = 0
+//loop nums[i].toString();
+//count number of digits in nums[i].toString()[j]
+//at end if even increment evens
+
+//return evens
+
+//rethought when coding:
+//nix the double loop just tag the length of nums[i].toString()
