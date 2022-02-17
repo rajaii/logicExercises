6ba4a32d14dfd9ee99d21d641ef7e11857b10775
Ali Rajaii <rajaii_a@yahoo.com>
2022-02-17 10:30:12 -0600

    pascals-triangleii.js O(n^2) runtime o(n^2) space

---
+++ javascript/pascals-triangleii.js
@@ -0,0 +1,31 @@
+//from leetcode easy: https://leetcode.com/problems/pascals-triangle-ii
+
+var getRow = function(rowIndex) {
+    //make a function generatePascal that generates 2d array up to row
+    function generatePascalAndPullRow(rowIndex) {
+        const pascal = [[1],];
+        for (let i = 0; i < rowIndex; i++) {
+            const newRow = [];
+            for (let j = 0; j < pascal[pascal.length - 1].length + 1; j++) {
+                if (j === 0) {
+                    newRow.push(1);
+                    continue;
+                } else if (j === pascal[pascal.length - 1].length) {
+                    newRow.push(1);
+                    pascal.push(newRow);
+                    break;
+                } else {
+                    const topLeft = pascal[pascal.length - 1][j - 1];
+                    const topRight = pascal[pascal.length - 1][j];
+                    newRow.push(topLeft + topRight);
+                }
+                
+            }
+        }
+        
+        return pascal[pascal.length - 1];
+    }
+    
+    
+    return generatePascalAndPullRow(rowIndex);
+};
