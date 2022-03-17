9acab868d8a5a6ace0554b28b5b5756ca20be4bc
Ali Rajaii <rajaii_a@yahoo.com>
2022-03-17 11:20:36 -0500

    refactor firsAndLastPosition.js o(n) o(1) space

--- javascript/findFirstAndLastPosition.js
+++ javascript/findFirstAndLastPosition.js
@@ -1,4 +1,4 @@
-// from leetcode easy: https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array
+// from leetcode medium: https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array
 
 var searchRange = function(n, t) {
     //handle edge case
@@ -42,3 +42,23 @@ var searchRange = function(n, t) {
 //retArr.push(i)
 //return retArr;
 //}
+
+//REFACTOR
+
+var searchRange = function(n, t) {
+const tA = [n.indexOf(t)];
+
+if (tA[0] === -1) {
+    return [-1,-1];
+}
+
+for (let i = tA[0]; i < n.length; i++) {
+    if (n[i] !== t) {
+        tA.push(i-1);
+        return tA;
+    } else if (i === n.length - 1) {
+        tA.push(n.length - 1);
+        return tA;
+    } 
+}
+};
