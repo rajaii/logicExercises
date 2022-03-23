62e0be70fe48d9bd375caddeaf3496a31090268f
Ali Rajaii <rajaii_a@yahoo.com>
2022-03-23 11:08:51 -0500

    diagonalSum.js o(n) space o(n) time

--- javascript/x.js
+++ javascript/x.js
@@ -3,12 +3,12 @@
 const x = 'w'
 console.log(isNaN(parseInt(x)))
 
-//refactor into bst, then study and refactor
-//refactor the easy after study
-//do next medium from bst
 
 
 
 
+const q = [4,2,3];
+let cur = q.pop()
 
-
+console.log(cur)
+console.log(cur)
