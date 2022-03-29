232816885338468d51c54ed30379bf4ed63e80f6
Ali Rajaii <rajaii_a@yahoo.com>
2022-03-29 10:57:47 -0500

    printWordsVertically.js o(n^2) runtime o(n) space

--- javascript/x.js
+++ javascript/x.js
@@ -1,1 +1,11 @@
-//  review diagonalSum and checkRowColHasN and reflect/refactor and then do next from checkRowColHasN
+//  rview/refactor 
+//==>
+//https://leetcode.com/problems/print-words-vertically/
+//and do or ones from ^:
+
+//https://leetcode.com/problems/spiral-matrix/
+const str = "TO BE OR NOT TO BE";
+const s = str.split(" ")
+console.log(s[4][2])
+
+
