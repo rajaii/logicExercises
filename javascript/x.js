943570dee367feaa8175808b32ab53fe449c391a
Ali Rajaii <rajaii_a@yahoo.com>
2022-05-05 10:30:17 -0500

    kthMissingPositive O(n^2) runtime o(n) space

--- javascript/x.js
+++ javascript/x.js
@@ -1,8 +1,14 @@
 
 //==> 
-//https://leetcode.com/problems/find-all-duplicates-in-an-array/ and/or meds from V
+//easy
+
+
+  //meds from V and study V
 // try https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
+//https://leetcode.com/problems/find-all-duplicates-in-an-array/
+const test = { "autopay_exclude": true, "autopay_exclusion_reason": "ali", "isKOOL": true, "otherprop": "last one"}
 
+const { autopay_exclude, autopay_exclusion_reason, ...any } = test;
 
-
+console.log(any)
 
