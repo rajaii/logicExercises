2de2ba4889e75fb61a0869578524eeb2696a5605
Ali Rajaii <rajaii_a@yahoo.com>
2022-05-12 10:47:27 -0500

    refactor using obj to make o(n) runtime o(n) space

--- javascript/findUniqueBinary.js
+++ javascript/findUniqueBinary.js
@@ -18,3 +18,38 @@ var findDifferentBinaryString = function(n) {
   }
   
 };
+//Refactor
+var findDifferentBinaryString = function(n) {
+  const list = [];
+  
+  const len = 2 ** n.length - 1;
+  
+  for (let i = 0; i < len + 1; i++) {
+      if (!n.includes(i.toString(2).padStart(n.length, "0"))) {
+          return i.toString(2).padStart(n.length, "0");
+      }
+  } 
+  
+};
+
+//refactor to o(n)
+var findDifferentBinaryString = function(n) {
+  const list = [];
+  let nObj = {};
+  
+  //make an object of the form n[i]: n[i];
+  for (let i = 0; i < n.length; i++) {
+      if (!nObj[n[i]]) {
+          nObj[n[i]] = n[i];
+      }
+  }
+  
+  const len = 2 ** n.length - 1;
+  
+  for (let i = 0; i < len + 1; i++) {
+      if (!nObj[i.toString(2).padStart(n.length, "0")]) {
+          return i.toString(2).padStart(n.length, "0");
+      }
+  } 
+  
+};
