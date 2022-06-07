555f58fd6009878a4ca66522d37c983a7c0d25ff
Ali Rajaii <rajaii_a@yahoo.com>
2022-06-07 10:50:00 -0500

    complimentOfBaseTen.js o(n) runtime o(1)space

--- javascript/complimentOfBaseTen.js
+++ javascript/complimentOfBaseTen.js
@@ -22,3 +22,16 @@ var bitwiseComplement = function(n) {
 //loop bin and flip
 
 //return parseInt(bin.join(""), 2);
+
+//refactor
+
+var bitwiseComplement = function(n) {
+  return parseInt(n.toString(2).split("").map(n => {
+      if (n === "1") {
+          return "0";
+      } else {
+          return "1";
+      }
+  }).join(""), 2);
+  
+};
--- javascript/x.js
+++ javascript/x.js
@@ -2,6 +2,7 @@
   //https://leetcode.com/problems/find-the-middle-index-in-array
   //https://leetcode.com/problems/find-pivot-index
   //https://leetcode.com/problems/number-of-ways-to-split-array
+  //https://leetcode.com/problems/complement-of-base-10-integer
 
 
 const i = "101";
