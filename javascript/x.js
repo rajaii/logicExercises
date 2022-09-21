514a364d6e07308144236a64695c9769b0e9e829
ali holt <rajaii_a@yahoo.com>
2022-09-21 10:29:43 -0700

    sortCharsByFreq.js o(n^2) time o(n) space

--- javascript/x.js
+++ javascript/x.js
@@ -4,9 +4,11 @@
 //https://leetcode.com/problems/find-triangular-sum-of-an-array/
 //https://leetcode.com/problems/minimum-rounds-to-complete-all-tasks/
 //https://leetcode.com/problems/k-closest-points-to-origin/
+//https://leetcode.com/problems/top-k-frequent-elements
 
 //==> follow up:
 //https://leetcode.com/problems/top-k-frequent-elements
+//https://leetcode.com/problems/sort-characters-by-frequency
 
 
 // function getStateName(abbrv) {
