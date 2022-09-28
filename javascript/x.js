049eae6ee7122c1bfc4eb0dc8af0767c544ebb44
Ali Rajaii <rajaii_a@yahoo.com>
2022-09-28 18:04:21 +0700

    containsDuplicate.js o(n log n) time o(1) space with o (n) (on both time and space) refactor

--- javascript/x.js
+++ javascript/x.js
@@ -4,38 +4,12 @@
 //https://leetcode.com/problems/find-triangular-sum-of-an-array/
 //https://leetcode.com/problems/minimum-rounds-to-complete-all-tasks/
 //https://leetcode.com/problems/k-closest-points-to-origin/
-//https://leetcode.com/problems/top-k-frequent-elements
-//https://leetcode.com/problems/top-k-frequent-words
-
-//==> follow up:
-//https://leetcode.com/problems/top-k-frequent-elements
 
 
+const names = [ 'Alice', 'alice' ]
+names.sort()
 
-// function getStateName(abbrv) {
-//   if (abbrv === "TX") {
-//       displayResult("Texas");
-//   } else if (abbrv === "CA") {
-//       displayResult("California");
-//   } else if (abbrv === "NY") {
-//       displayResult("New York");
-//   } 
-// }
+console.log(names);
 
-// const stateName = getStateName("ZZ");
-// console.log(stateName)
-
-
-
-
-const a = ["a", "c", "b", "z", "d", "j"]
-a.sort((a,b) => {
-  if (a < b) {
-    return -1;
-  }
-}
-)
-
-console.log(a);
 
 
