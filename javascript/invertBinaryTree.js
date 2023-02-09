d006332a6b86d206de05b6c777df0dd7228b0851
Ali Rajaii <rajaii_a@yahoo.com>
2023-02-08 23:49:53 -0800

    invertBinaryTree.js o(n*2) runtime o(n) space

---
+++ javascript/invertBinaryTree.js
@@ -0,0 +1,34 @@
+//  from leetcode easy: https://leetcode.com/problems/invert-binary-tree/
+
+var invertTree = function(root) {
+  if (root === null) {
+      return root
+  }
+  const queue = [root];
+  const visited = [];
+
+  while (queue.length > 0) {
+      if (queue[0].left) {
+          queue.push(queue[0].left);
+      }
+      if (queue[0].right) {
+          queue.push(queue[0].right);
+      }
+      
+      if (!visited.includes(queue[0])) {
+          const tempRight = queue[0].right;
+          queue[0].right = queue[0].left;
+          queue[0].left = tempRight;
+          visited.push(queue[0]);
+      }
+
+      queue.shift();
+  }
+
+  console.log(root)
+
+  return root;
+};
+
+//traverse BFT quque
+//set cur.left to cur.right vice versa
