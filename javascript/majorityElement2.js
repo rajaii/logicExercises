8492a4b9ba9cabbef7ca2480baab3ef38b3df4fc
Ali Rajaii <rajaii_a@yahoo.com>
2023-02-28 20:19:37 -0800

    majorityElement2 o(n log n) time o(n) space

--- javascript/majorityElement2.js
+++ javascript/majorityElement2.js
@@ -28,3 +28,34 @@ var majorityElement = function(nums) {
 
   return elements;
 };
+
+//Refactor to o (n log n):
+var majorityElement = function(nums) {
+  if (nums.length === 1) {
+      return nums;
+  }
+  const limit = nums.length / 3;
+  nums.sort((a,b) => a - b);
+
+  const elements = [];
+  let count = 1;
+  for (let i = 1; i < nums.length; i++) {
+      if (nums[i] !== nums[i-1]) {
+          if (count > limit) {
+              elements.push(nums[i -1]);
+          }
+          count = 0;
+      }
+
+      count += 1;
+
+      
+      if (i === nums.length - 1 && count > limit) {
+          elements.push(nums[i]);
+      } 
+
+
+  }
+
+  return elements;
+};
