9e8c85205255951798c7e9bdb930e9b5d4cefc67
Ali Rajaii <rajaii_a@yahoo.com>
2023-02-28 21:29:08 -0800

    sorting algos

---
+++ notes/bubbleSort.js
@@ -0,0 +1,22 @@
+//make a double loop passing and swapping on the inner if j > j+1
+//use a  boolean to flip if swapped
+//when full pass w/o swap return array or break
+
+var sortArray = function(n) {
+  //bubble sort:
+  for (let i = 0; i < n.length; i++) {
+      let swapped = false;
+      for (let j = 0; j < n.length; j++) {
+          if (n[j] > n[j + 1]) {
+              swapped = true;
+              let temp = n[j];
+              n[j] = n[j+1];
+              n[j+1] = temp;
+          }
+          if (j === n.length - 1 && !swapped) {
+              return n;
+          }
+      }
+  }
+};
+
