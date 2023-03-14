016bb0615a5b4b10008fbe7fb9f2916d5e251db5
Ali Rajaii <rajaii_a@yahoo.com>
2023-03-13 22:50:00 -0700

    iterative

--- coderpro/js/recursionPractice/reverseStringRecursion.js
+++ coderpro/js/recursionPractice/reverseStringRecursion.js
@@ -25,30 +25,44 @@
 //   }
 // }
 
-// Iterative
+//Recursive following the iterative flow
+// class Solution {
+//   constructor (string) {
+//     this.string = string;
+//   }
+
+//   helperReverse (string, toAppend) {
+//     if (!string.length) {
+//       return toAppend;
+//     }
+
+//     toAppend += string[string.length - 1];
+//     const toPass = string.slice(0, string.length - 1);
+
+//     return this.helperReverse(toPass, toAppend);
+//   }
+
+//   reverse () {
+//     return this.helperReverse(this.string, "");
+//   }
+// }
+
+
+//Recursive following the iterative flow o(n) time and space
+
 class Solution {
   constructor (string) {
     this.string = string;
   }
 
-  helperReverse (string, toAppend) {
-    if (!string.length) {
-      return toAppend;
-    }
-
-    toAppend += string[string.length - 1];
-    const toPass = string.slice(0, string.length - 1);
-
-    return this.helperReverse(toPass, toAppend);
-  }
-
   reverse () {
-    return this.helperReverse(this.string, "");
+    let toAppend = "";
+    for (let i = this.string.length - 1; i > -1; i--) {
+      toAppend += this.string[i];
+    } 
+    return toAppend;
   }
 }
-
-
-//Recursive following the iterative flow
 
 const s = new Solution("aaakkkddd");
 
