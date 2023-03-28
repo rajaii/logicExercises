d2a23a39623436198d65cbbac3d53adefa09f08f
Ali Rajaii <rajaii_a@yahoo.com>
2023-03-27 21:55:49 -0700

    tournamentWinner.js first sln

---
+++ algoexpert/tournamentWinner.js
@@ -0,0 +1,28 @@
+//first solution o(n) time o(n) space
+function tournamentWinner(competitions, results) {
+  //get object of the form {name: 0}
+  let stats = {};
+
+  for (let i = 0; i < competitions.length; i++) {
+    stats[competitions[i][0]] = 0;
+    stats[competitions[i][1]] = 0;
+  }
+
+  //loop competitions and results and iterate the wins accordingly
+  for (let i = 0; i < competitions.length; i++) {
+     if (results[i] === 0) {
+      stats[competitions[i][1]] += 3
+    } else {
+      stats[competitions[i][0]] += 3
+    }
+  }
+
+  //find max and return the key of the max in stats
+  const max = Math.max(...Object.values(stats));
+  for (let key in stats) {
+    if (stats[key] === max) {
+      return key;
+    }
+  }
+
+}
