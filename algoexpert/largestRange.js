a6509ab1a28aabd43432d6325f303cf37ea8d77d
Ali Rajaii <rajaii_a@yahoo.com>
2023-04-02 20:56:02 -0700

    largestRange.js refactor

--- algoexpert/largestRange.js
+++ algoexpert/largestRange.js
@@ -39,3 +39,51 @@ function largestRange(array) {
 
   return [largest[0], largest[largest.length - 1]];
 }
+
+//second solution (after hints and some of video) o(n) time o(n) space
+
+function largestRange(array) {
+  if (array.length === 1) {
+    return [array[0], array[0]];
+  }
+  //store in hash table
+  const nums = {}
+  for (let i = 0; i < array.length; i++) {
+    nums[array[i]] = null;
+  }
+
+  let largest = [0,0];
+  //loop array and check with hash table
+  for (let i = 0; i < array.length; i++) {
+    if (!nums[array[i]]) {
+      nums[array[i]] = 'visited';
+    
+      const tempLargest = []
+      let tempLg = array[i];
+      let j = 1;
+      while (nums[array[i] + j] === null) {
+        nums[array[i] + j] = 'visited';
+        tempLg = array[i] + j;
+        j += 1;
+      }
+      tempLargest[1] = tempLg;
+
+      j = 1;
+      let tempSm = array[i];
+      while (nums[array[i] - j] === null) {
+        nums[array[i] - j] = 'visited';
+        tempSm = array[i] - j;
+        j += 1;
+      }
+      tempLargest[0] = tempSm;
+
+      if (tempLargest[1] - tempLargest[0] > largest[1] - largest[0]) {
+        largest = tempLargest;
+      }
+    }
+  }
+
+
+  return largest;
+  
+}
