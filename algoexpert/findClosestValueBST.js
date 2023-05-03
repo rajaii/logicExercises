9712749d9343040da60f3ecf6b80a2ecca9d787a
Ali Rajaii <rajaii_a@yahoo.com>
2023-05-02 20:14:12 -0700

    findClosestVAlueVBST o(n) now

--- algoexpert/findClosestValueBST.js
+++ algoexpert/findClosestValueBST.js
@@ -1,6 +1,35 @@
 //Easy:
 
-//first sln o(n) time and space using bfs iterative
+//first sln o(n ^ 2) time and space using bfs iterative
+function findClosestValueInBst(tree, target) {
+  //store min
+  let min = Infinity;
+  value = null;
+
+  //traverse bst 
+  const queue = [tree];
+  const visited = [tree];
+
+  while (queue.length) {
+    if (queue[0] && !visited.includes(queue[0].value)) {
+      if (Math.abs(queue[0].value - target) < min) {
+      min = Math.abs(queue[0].value - target);
+      value = queue[0].value
+    }
+    
+      queue.push(queue[0].left);
+      queue.push(queue[0].right);
+    }
+
+    queue.shift();
+  }
+
+  return value;
+}
+
+
+
+//using class and recursion
 
 function findClosestValueInBst(tree, target) {
   class TraverseAndFind {
@@ -31,4 +60,32 @@ function findClosestValueInBst(tree, target) {
   }
 
   return new TraverseAndFind().traverse();
+}
+
+//second sln o(n) time and space using set to improve time
+function findClosestValueInBst(tree, target) {
+  //store min
+  let min = Infinity;
+  value = null;
+
+  //traverse bst 
+  const queue = [tree];
+  const visited = new Set();
+  visited.add(tree);
+
+  while (queue.length) {
+    if (queue[0] && !visited.has(queue[0].value)) {
+      if (Math.abs(queue[0].value - target) < min) {
+      min = Math.abs(queue[0].value - target);
+      value = queue[0].value
+    }
+      visited.add(queue[0]);
+      queue.push(queue[0].left);
+      queue.push(queue[0].right);
+    }
+
+    queue.shift();
+  }
+
+  return value;
 }
