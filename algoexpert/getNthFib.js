83fb823ccff3c8df96d1639833e768b8d5b94823
Ali Rajaii <alirajaii@alis-mbp.myfiosgateway.com>
2023-06-22 10:43:33 -0400

    getNthFib.js

--- algoexpert/getNthFib.js
+++ algoexpert/getNthFib.js
@@ -19,3 +19,37 @@ function getNthFib(n) {
 
   return value;
 }
+
+//solution 2 recusion self done
+
+function getNthFib(n) {
+  if (n === 1) {
+    return 0;
+  }
+
+  function helper(num, prevs, value) {
+    if (num === n) {
+      return value;
+    }
+
+    value = prevs[0] + prevs[1];
+    prevs[0] = prevs[1];
+    prevs[1] = value;
+    num += 1;
+    return helper(num, prevs, value);
+  }
+
+  return helper(2, [0, 1], 1);
+}
+
+//solution 3 recursion from algo expert
+
+function getNthFib(n) {
+  if (n === 1) {
+    return 0;
+  }
+
+  if (n === 2) {
+    return 1;
+  } else return getNthFib(n - 1) + getNthFib(n - 2);
+}
