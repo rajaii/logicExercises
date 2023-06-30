c863c5a6dd395cef22166618e927b2220a0d69d5
Ali Rajaii <alirajaii@alis-mbp.myfiosgateway.com>
2023-06-30 09:20:04 -0400

    isPalindrome.js

--- algoexpert/isPlindrome.js
+++ algoexpert/isPlindrome.js
@@ -19,3 +19,31 @@ function isPalindrome(string) {
 
   return true;
 }
+
+//recursion
+function isPalindrome(string) {
+  if (string.length <= 1) {
+    return true; // Base case: empty string or single character is a palindrome
+  }
+
+  if (string[0] !== string[string.length - 1]) {
+    return false; // First and last characters don't match, not a palindrome
+  }
+
+  // Recursively check the substring without the first and last characters
+  return isPalindrome(string.slice(1, string.length - 1));
+}
+
+//recursion o(n) time o(n) space bc call Stack
+function isPalindrome(string) {
+  if (string.length <= 1) {
+    return true; // Base case: empty string or single character is a palindrome
+  }
+
+  if (string[0] !== string[string.length - 1]) {
+    return false; // First and last characters don't match, not a palindrome
+  }
+
+  // Recursively check the substring without the first and last characters
+  return isPalindrome(string.slice(1, string.length - 1));
+}
