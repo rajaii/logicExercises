9a9ac54531b517e227441000618def20f2261ec5
Ali Rajaii <alirajaii@alis-mbp.myfiosgateway.com>
2023-07-19 11:56:36 -0400

    sumOfLinkedLists

--- algoexpert/sumOfLinkedLists.js
+++ algoexpert/sumOfLinkedLists.js
@@ -31,3 +31,36 @@ function sumOfLinkedLists(linkedListOne, linkedListTwo) {
 
   return newHead;
 }
+
+//got rid of the string concatenation
+
+function sumOfLinkedLists(linkedListOne, linkedListTwo) {
+  let cur1 = linkedListOne;
+  let num1 = [];
+  while (cur1 !== null) {
+    num1.push(cur1.value.toString());
+    cur1 = cur1.next;
+  }
+
+  let cur2 = linkedListTwo;
+  let num2 = [];
+  while (cur2 !== null) {
+    num2.push(cur2.value.toString());
+    cur2 = cur2.next;
+  }
+  const numStr1 = num1.reverse().join('');
+  const numStr2 = num2.reverse().join('');
+
+  let sum = parseInt(numStr1) + parseInt(numStr2);
+  let digs = sum.toString().split('').reverse();
+
+  const newHead = new LinkedList(parseInt(digs[0]));
+  let finCur = newHead;
+  for (let i = 1; i < digs.length; i++) {
+    finCur.next = new LinkedList(parseInt(digs[i]));
+    finCur = finCur.next;
+  }
+  finCur.next = null;
+
+  return newHead;
+}
