132ef5e9585085dcc72e621127e94ec5b6fc970a
Ali Rajaii <alirajaii@alis-mbp.myfiosgateway.com>
2023-07-20 10:39:53 -0400

    sumOfLinkedLists.js

--- algoexpert/sumOfLinkedLists.js
+++ algoexpert/sumOfLinkedLists.js
@@ -64,3 +64,100 @@ function sumOfLinkedLists(linkedListOne, linkedListTwo) {
 
   return newHead;
 }
+
+//Same time but 0(1 space):
+function sumOfLinkedLists(linkedListOne, linkedListTwo) {
+  let toLoop;
+  let other;
+  let count1 = 0;
+  let cur = linkedListOne;
+  while (cur !== null) {
+    count1 += 1;
+    cur = cur.next;
+  }
+
+  let count2 = 0;
+  let curTwo = linkedListTwo;
+  while (curTwo !== null) {
+    count2 += 1;
+    curTwo = curTwo.next;
+  }
+
+  if (count1 >= count2) {
+    toLoop = linkedListOne;
+    other = linkedListTwo;
+  } else {
+    toLoop = linkedListTwo;
+    other = linkedListOne;
+  }
+
+  const dummy = new LinkedList(0);
+  let current = dummy;
+  let cur1 = toLoop;
+  let cur2 = other;
+  let carry = 0;
+  while (cur1 !== null) {
+    if (cur2) {
+      if (cur1.value + cur2.value + carry > 9) {
+        current.next = new LinkedList(
+          parseInt((cur1.value + cur2.value + carry).toString()[1])
+        );
+        carry = 1;
+      } else if (cur1.value + cur2.value + carry <= 9) {
+        current.next = new LinkedList(cur1.value + cur2.value + carry);
+        carry = 0;
+      }
+      cur1 = cur1.next;
+      cur2 = cur2.next;
+      current = current.next;
+    } else {
+      if (cur1.value + carry > 9) {
+        current.next = new LinkedList(
+          parseInt((cur1.value + carry).toString()[1])
+        );
+        carry = 1;
+      } else {
+        current.next = new LinkedList(cur1.value + carry);
+        carry = 0;
+      }
+
+      cur1 = cur1.next;
+      current = current.next;
+    }
+  }
+
+  if (carry) {
+    current.next = new LinkedList(1);
+  }
+
+  return dummy.next;
+}
+
+//cleaner:
+function sumOfLinkedLists(linkedListOne, linkedListTwo) {
+  const dummy = new LinkedList(0);
+  let current = dummy;
+  let carry = 0;
+
+  let cur1 = linkedListOne;
+  let cur2 = linkedListTwo;
+
+  while (cur1 || cur2) {
+    const val1 = cur1 ? cur1.val : 0;
+    const val2 = cur2 ? cur2.val : 0;
+    const sum = val1 + val2 + carry;
+
+    carry = Math.floor(sum / 10);
+    current.next = new LinkedList(sum % 10);
+
+    current = current.next;
+    if (cur1) cur1 = cur1.next;
+    if (cur2) cur2 = cur2.next;
+  }
+
+  if (carry) {
+    current.next = new LinkedList(carry);
+  }
+
+  return dummy.next;
+}
