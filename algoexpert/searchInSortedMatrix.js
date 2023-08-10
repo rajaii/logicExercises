6a8ccd56596ca7f20c1423e444e62c32ec04b031
Ali Rajaii <alirajaii@alis-mbp.myfiosgateway.com>
2023-08-10 06:09:32 -0400

    fourNumberSum.js

--- algoexpert/searchInSortedMatrix.js
+++ algoexpert/searchInSortedMatrix.js
@@ -22,3 +22,19 @@ function searchInSortedMatrix(matrix, target) {
 
   return ret;
 }
+
+//same time/space cleaner
+function searchInSortedMatrix(matrix, target) {
+  let row = 0;
+  let col = matrix[0].length - 1;
+  while (row < matrix[0].length - 1 && col >= 0) {
+    if (matrix[row][col] > target) {
+      col -= 1;
+    } else if (matrix[row][col] < target) {
+      row += 1;
+    } else {
+      return [row, col];
+    }
+  }
+  return [-1, -1];
+}
