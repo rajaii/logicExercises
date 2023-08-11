2a6035d511cbc705184fcf7559862f1da0a4b5ca
Ali Rajaii <alirajaii@alis-mbp.myfiosgateway.com>
2023-08-11 07:28:11 -0400

    selectionSort.js

--- javascript/x.js
+++ javascript/x.js
@@ -20,7 +20,7 @@
 //if stuck more than 30 min of that look at answer, reproduce from that, move on and put in second "later" list to return
 
 //==>
-//  easy any, med any, hard, very hard
+//  selection sort, med any, hard, very hard
 
 //learn what recursion does undeer the hood as I am getting good with using it
 
