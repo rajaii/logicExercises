95be77fd88c6bc601d6283833e0bb80fedfbc462
Ali Rajaii <alirajaii@alis-mbp.myfiosgateway.com>
2023-08-16 07:13:55 -0400

    quickSort.js

---
+++ algoexpert/quickSort.js
@@ -0,0 +1,43 @@
+//HARD:
+
+//o(n log n) time o(n) space:
+function quickSort(array) {
+  if (array.length <= 1) {
+    return array;
+  }
+
+  const pivot = array[0]; // Using the first element as pivot
+  const left = [];
+  const right = [];
+
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] < pivot) {
+      left.push(array[i]);
+    } else {
+      right.push(array[i]);
+    }
+  }
+
+  return [...quickSort(left), pivot, ...quickSort(right)];
+}
+
+//same complexity using end for pivot
+function quickSort(array) {
+  if (array.length <= 1) {
+    return array;
+  }
+
+  const pivot = array[array.length - 1];
+  const left = [];
+  const right = [];
+
+  for (let i = 0; i < array.length - 1; i++) {
+    if (array[i] < pivot) {
+      left.push(array[i]);
+    } else {
+      right.push(array[i]);
+    }
+  }
+
+  return [...quickSort(left), pivot, ...quickSort(right)];
+}
