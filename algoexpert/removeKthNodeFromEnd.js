6e4251e420217c7c5eb6c0df4737df3766314fa0
Ali Rajaii <alirajaii@alis-mbp.myfiosgateway.com>
2023-08-31 09:48:15 -0400

    removeKthNodeFromEnd.js

--- algoexpert/removeKthNodeFromEnd.js
+++ algoexpert/removeKthNodeFromEnd.js
@@ -31,3 +31,29 @@ function removeKthNodeFromEnd(head, k) {
   }
   return head;
 }
+
+//same time/space different spin
+function removeKthNodeFromEnd(head, k) {
+  let s = head;
+  let f = head;
+  for (let i = 0; i < k; i++) {
+    s = s.next;
+  }
+
+  if (s === null) {
+    head.value = head.next.value;
+    head.next = head.next.next;
+  }
+
+  let prev = null;
+  while (s !== null) {
+    prev = f;
+    s = s.next;
+    f = f.next;
+    if (s === null) {
+      prev.next = f.next;
+    }
+  }
+
+  return head;
+}
